c37a4a526d98467b3b8a29ce91719214b29686ce
Sem Voigtländer <43578852+userlandkernel@users.noreply.github.com>
2019-08-30 23:30:25 +0200

    Create saveblobs.sh

---
+++ saveblobs.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env php
+
+// Uses PHP and curl to get blobs for the specified device straight from Apple's TSS Servers
+// Also can get a list of stored blobs on shsh.host and conan's TSS server for the specified device.
