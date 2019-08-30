8c18708366df016ce8932945be3fb63df591d35c
Sem Voigtländer <43578852+userlandkernel@users.noreply.github.com>
2019-08-30 23:24:17 +0200

    Create getxnu.sh

---
+++ getxnu.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Work in progress
+
+# -- latest (-l)
+# Downloads latest XNU tarbal into the tarballs/ directory
+
+# --release (-r)
+# Downloads the specified release tarball into a directory named after the version in the tarballs/ directory
+
+# -- extract (-e)
+# Extracts the tarball from either -l or -r into a folder named after its version in the sources/ directory
+
+# -- sync
+# Synchronises the sourcecode to the url set in the REPO_URL directory
+
